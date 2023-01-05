c1687fe391c63007e7a744a0a86964ceda616159
jaydeepdhrangiya-admin <recentpagetoday@gmail.com>
2023-01-05 08:39:11 +0700

    ooakdk

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+      <ToastContainer />
+    </SessionProvider>
+  );
 }
---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,46 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+// Create a new interface for the user
+interface User {
+  name?: string | null | undefined;
+  email?: string | null | undefined;
+  image?: string | null | undefined;
+  id?: string | null | undefined;
+}
+
+const prisma = new PrismaClient();
+
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      // The client ID and secret for your GitHub app authentication
+      //@ts-ignore
+      clientId: process.env.GITHUB_CLIENT_ID,
+      //@ts-ignore
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async session({ session, token }) {
+      session = {
+        ...session,
+        user: {
+          ...session.user,
+          id: token?.sub,
+        } as User,
+      };
+      return session;
+    },
+  },
+  session: {
+    strategy: "jwt",
+    //@ts-ignore
+    jwt: true,
+  },
+  debug: false,
+});
