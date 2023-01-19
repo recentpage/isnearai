1ea9c4e5d4a43322a0288ec2ab53f7cd0a89bf38
jaydeepdhrangiya-admin <recentpagetoday@gmail.com>
2023-01-19 14:08:13 +1000

    ok

---
+++ pages/api/genration/permeneantlydeletegenset/[id].ts
@@ -0,0 +1,27 @@
+// delete tool from copytool
+
+import { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/react";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const session = await getSession({ req });
+  const { id } = req.query;
+
+  if (session) {
+    const tool = await prisma.copygen.delete({
+      where: {
+        id: id as string,
+      },
+    });
+
+    res.json({ status: "success" });
+  } else {
+    res.status(401).json({ error: "Unauthorized" });
+  }
+}
